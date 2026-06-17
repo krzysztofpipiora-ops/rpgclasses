86af12303f83e3a7d6e7cbfde4ba033361b6d992
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-06-17 19:42:15 +0200

    Update AspectCommand.java

--- src/main/java/com/example/rpgaspects/AspectCommand.java
+++ src/main/java/com/example/rpgaspects/AspectCommand.java
@@ -4,6 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Bukkit行政;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public class AspectCommand implements CommandExecutor {
@@ -21,53 +23,10 @@ public class AspectCommand implements CommandExecutor {
             return true;
         }
 
-        PlayerData data = plugin.getPlayerData(player);
-
-        if (args.length == 0) {
-            player.sendMessage("§6=== SYSTEM ASPEKTOW RPG ===");
-            player.sendMessage("§eTwor Poziom RPG: §f" + data.getLevel());
-            player.sendMessage("§eAktywny Aspekt: §a" + data.getCurrentAspect().getName());
-            player.sendMessage("§7Wpisz: §b/aspekt wybierz [NAZWA]§7 aby zmienic klase.");
-            player.sendMessage("§7Dostepne aspekty:");
-            for (PlayerData.AspectType type : PlayerData.AspectType.values()) {
-                if (type == PlayerData.AspectType.NONE) continue;
-                player.sendMessage("§e- §f" + type.name() + " §7(Wymaga poziomu: " + type.getMinLevel() + ") - " + type.getDescription());
-            }
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("wybierz") && args.length > 1) {
-            String wybrany = args[1].toUpperCase();
-
-            try {
-                PlayerData.AspectType aspect = PlayerData.AspectType.valueOf(wybrany);
-                
-                if (data.getLevel() < aspect.getMinLevel()) {
-                    player.sendMessage("§cNie posiadasz wystarczajacego poziomu! Wymagany poziom: " + aspect.getMinLevel());
-                    return true;
-                }
-
-                data.setCurrentAspect(aspect);
-                player.sendMessage("§aZmieniono aspekt na: §2" + aspect.getName());
-                player.sendMessage("§7Zdolnosc: " + aspect.getDescription());
-
-            } catch (IllegalArgumentException e) {
-                player.sendMessage("§cNiepoprawna nazwa aspektu! Sprawdz liste pod /aspekt");
-            }
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("setlevel") && args.length > 1 && player.isOp()) {
-            try {
-                int targetLevel = Integer.parseInt(args[1]);
-                data.setLevel(targetLevel);
-                player.sendMessage("§a[Cheat] Ustawiono Twoj poziom na: " + targetLevel);
-            } catch (NumberFormatException e) {
-                player.sendMessage("§cPodaj poprawna liczbe.");
-            }
-            return true;
-        }
-
+        // Pobieramy instancję listenera, aby wywołać otwarcie stworzonego GUI
+        AspectListener listener = new AspectListener(plugin);
+        listener.openAspectGUI(player);
+        
         return true;
     }
 }
